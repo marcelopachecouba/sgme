b5f283de5659813beb2d313e47665312a8e6867d
Marcelo Pacheco <marcelosouzapacheco@gmail.com>
2026-02-28 18:12:27 -0300

    Adicionando rota init-db

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","sqlite:///sgme.db")
+    SQLALCHEMY_ENGINE_OPTIONS = {"connect_args":{"sslmode":"require"}}
