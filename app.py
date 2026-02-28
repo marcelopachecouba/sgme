5f92a1bd221d2ff20691093bcb6d9742627d1589
Marcelo Pacheco <marcelosouzapacheco@gmail.com>
2026-02-28 20:53:55 -0300

    Adicionando rota init-db

--- app.py
+++ app.py
@@ -1339,5 +1339,6 @@ def escala_publica(token):
 # ======================
 
 if __name__ == "__main__":
-    app.run()
+    port= int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
 
