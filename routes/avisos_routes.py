dc5eebb077bafab49cdab44118b5ae600cc01e35
Marcelo Pacheco <marcelosouzapacheco@gmail.com>
2026-03-05 17:07:04 -0300

    corrige rota excluir ministro

---
+++ routes/avisos_routes.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from models import db, Aviso
+import os
+
+avisos_bp = Blueprint("avisos", __name__)
+
+UPLOAD_FOLDER = "static/uploads"
+
+
+@avisos_bp.route("/avisos")
+def avisos():
+
+    lista = Aviso.query.order_by(Aviso.data.desc()).all()
+
+    return render_template("avisos.html", avisos=lista)
+
+
+@avisos_bp.route("/avisos/novo", methods=["GET","POST"])
+def novo_aviso():
+
+    if request.method == "POST":
+
+        titulo = request.form["titulo"]
+        mensagem = request.form["mensagem"]
+        tipo = request.form["tipo"]
+        video_url = request.form.get("video_url")
+
+        fixado = True if request.form.get("fixado") else False
+
+        arquivo_nome = None
+
+        arquivo = request.files.get("arquivo")
+
+        if arquivo and arquivo.filename != "":
+
+            nome = secure_filename(arquivo.filename)
+
+            caminho = os.path.join("static/uploads", nome)
+
+            arquivo.save(caminho)
+
+            arquivo_nome = nome
+
+        aviso = Aviso(
+            titulo=titulo,
+            mensagem=mensagem,
+            tipo=tipo,
+            arquivo=arquivo_nome,
+            video_url=video_url,
+            fixado=fixado
+        )
+
+        db.session.add(aviso)
+        db.session.commit()
+
+        return redirect(url_for("avisos.avisos"))
+
+    return render_template("novo_aviso.html")
